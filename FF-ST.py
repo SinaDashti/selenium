99f851f7cc0e1209f9d0a25b18180a0392131252
Sina Dashti <sina.v.dashti@gmail.com>
2020-01-10 09:48:41 +0100

    waiting for current server element

--- FF-ST.py
+++ FF-ST.py
@@ -6,16 +6,22 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 driver.get("https://www.speedtest.net")
-try:
-    element = WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.XPATH, "//*[@id='_evidon-banner-cookiebutton']"))
-    )
 
-    if element:
-        driver.\
-        find_element_by_xpath("//*[@id='_evidon-banner-cookiebutton']").click()
-        driver.find_element_by_xpath("//*[@id='_evidon-l3']/button").click()
+# Waiting for banners and dismiss it
+try:
+    WebDriverWait(driver, 30).\
+    until(EC.element_to_be_clickable\
+    ((By.XPATH, "//*[@id='_evidon-banner-cookiebutton']"))).click()
+    driver.find_element_by_xpath("//*[@id='_evidon-l3']/button").click()
 except Exception as e:
     print(e)
-finally:
-    driver.close()
+
+# waiting for page to be loaded and shows the current server element
+try:
+    WebDriverWait(driver, 30).\
+    until(EC.presence_of_element_located\
+    ((By.XPATH, "//div[@data-view-name='currentServer']/div[2]/a")))
+except Exception as e:
+    raise
+
+driver.close()
